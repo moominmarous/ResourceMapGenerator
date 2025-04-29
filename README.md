1a4b0e12976ae4f7a9ef3d696e93c1669a67080b
Maria Movsheva <foxynin4eva@gmail.com>
2025-04-29 00:19:47 -0400

    generate a map and draw it

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ResourceMapGenerator
+
+Create a 1D array representation of a  `width` x `height` matrix. Cell size can be adjusted.
+
+Future things to add: Save to file and load from file. 
---
+++ map.js
@@ -0,0 +1,50 @@
+const COLORS = Object.freeze(['#7B93AB', '#73B7FF', '#A09A44', '#496643']);
+
+const DIMENSIONS = {};
+
+const getDimensions = (flag) => {
+    if (flag != 0) {
+        document.getElementById("cell-input").value = Math.floor(Math.random() * 9) + 1;
+        document.getElementById("width-input").value = Math.floor(Math.random() * 9) + 1;
+        document.getElementById("height-input").value = Math.floor(Math.random() * 9) + 1;
+    }
+    DIMENSIONS.cell_size = document.getElementById("cell-input").value;
+    DIMENSIONS.map_w = document.getElementById("width-input").value * DIMENSIONS.cell_size;
+    DIMENSIONS.map_h = document.getElementById("height-input").value * DIMENSIONS.cell_size;
+    console.log(DIMENSIONS);
+}
+
+const drawMap = (map, container) => {
+    for (let i = 0; i < map.length; i++) {
+        let element = map[i];
+        let unit = document.createElement('div')
+        unit.className = 'cell-unit';
+        unit.width = container.width / DIMENSIONS.map_w;
+        unit.height = unit.width
+        unit.style.backgroundColor = COLORS[element];
+        if (i % DIMENSIONS.cell_size == 0) {
+            unit.style.borderLeft = '1px solid #ccc';
+        }
+        if (Math.ceil(((i + 1) / DIMENSIONS.map_w)) % DIMENSIONS.cell_size == 0) {
+            unit.style.borderBottom = '1px solid #ccc';
+        }
+        container.appendChild(unit);
+    }
+}
+
+const generateMap = (flag) => {
+    getDimensions(flag);
+    //make map
+    let map = new Array(DIMENSIONS.map_w * DIMENSIONS.map_h);
+    for (let i = 0; i < map.length; i++) {
+        map[i] = Math.floor(Math.random() * 4); // Assign random values to each element
+    }
+    let container = document.getElementById('map-space');
+    container.innerHTML = ''; //clear
+    console.log(container);
+    container.style.gridTemplateColumns = `repeat(${DIMENSIONS.map_w}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${DIMENSIONS.map_h}, 1fr)`;
+    container.style.width = `${20 * DIMENSIONS.map_w}px`;
+    container.style.height = `${20 * DIMENSIONS.map_h}px`;
+    drawMap(map, container);
+};
